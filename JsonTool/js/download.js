db41c4a61472bd9df5eea28525286705f953116d
Saied <57135128+RSaied@users.noreply.github.com>
2024-08-17 03:20:11 +0300

    update JsonTool

---
+++ JsonTool/js/download.js
@@ -0,0 +1,73 @@
+document.getElementById('btn-download').addEventListener('click', function () {
+const progressBar = document.getElementById('progress-bar2');
+const progressBarInner = document.getElementById('progress-bar2-inner');
+const progressText = document.getElementById('progress-text');
+const speedText = document.getElementById('speed-text');
+const remainingTimeText = document.getElementById('remaining-time-text');
+const downloadUrl = 'https://RSaied.github.io/JsonTool/JSON Tool {V0.24} By RSaied.github.io.apk';
+const downloadName = 'JSON Tool {V0.24} By RSaied.github.io.apk';
+
+progressBar.style.display = 'block';
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', downloadUrl, true);
+xhr.responseType = 'blob';
+
+let startTime = null;
+let previousLoaded = 0;
+let totalBytes = 0; // Total size of the file
+let remainingBytes = 0;
+
+xhr.onprogress = function (event) {
+if (event.lengthComputable) {
+if (!startTime) {
+startTime = new Date().getTime();
+totalBytes = event.total;
+}
+
+const currentTime = new Date().getTime();
+const elapsedTime = (currentTime - startTime) / 1000; // seconds
+const loaded = event.loaded;
+remainingBytes = totalBytes - loaded;
+
+const percentComplete = (loaded / totalBytes) * 100;
+
+progressBarInner.style.width = percentComplete + '%';
+progressText.innerText = 'الرجاء عدم إغلاق الصفحة حتى إنتهاء التحميل\n وصل إلى : ' + Math.round(percentComplete) + '%';
+
+const speed = (loaded - previousLoaded) / elapsedTime; // bytes per second
+previousLoaded = loaded;
+startTime = currentTime;
+
+const speedKbps = (speed * 8 / 1024).toFixed(2); // kbps
+speedText.innerText = 'السرعة: ' + speedKbps + ' kbps';
+
+const remainingTime = (remainingBytes / speed).toFixed(0); // seconds
+const minutes = Math.floor(remainingTime / 60);
+const seconds = remainingTime % 60;
+remainingTimeText.innerText = 'الوقت المتبقي: ' + minutes + ' دقيقة و ' + seconds + ' ثانية';
+}
+};
+
+xhr.onload = function () {
+if (xhr.status === 200) {
+const url = window.URL.createObjectURL(xhr.response);
+const a = document.createElement('a');
+a.href = url;
+a.download = downloadName;
+document.body.appendChild(a);
+a.click();
+a.remove();
+
+progressText.innerText = 'اكتمل تحميل التطبيق. قم بفتح الملف من مجلد التنزيلات';
+} else {
+progressText.innerText = 'حدث خطأ أثناء تحميل التطبيق. الرجاء المحاولة مرة أخرى.';
+}
+};
+
+xhr.onerror = function () {
+progressText.innerText = 'حدث خطأ أثناء تحميل التطبيق. الرجاء المحاولة مرة أخرى.';
+};
+
+xhr.send();
+});
